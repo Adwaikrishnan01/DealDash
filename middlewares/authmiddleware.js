2203e578c4f460b0ee8ea782e2f1f1455ba31a78
adwai <adwaikrishnan11@gmail.com>
2023-06-30 14:00:06 +0530

    backend

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -2,35 +2,35 @@ import JWT from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import AsyncHandler from "express-async-handler";
 
-export const requireSignin=AsyncHandler(async (req,res,next)=>{
+export const requireSignin = AsyncHandler(async (req, res, next) => {
     let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try{ 
-            token=req.headers.authorization.split(" ")[1]
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(" ")[1]
             const decoded = JWT.verify(token, process.env.JWT_SECRET);
-            console.log("decoded:",decoded);
+            console.log("decoded:", decoded);
             req.user = await userModel.findById(decoded._id)
-       
+
             next()
         } catch (error) {
             console.log(error);
             res.status(401);
-            throw new Error('Not authorized, token fail')
+            throw new Error('Not authorized,token fail')
         }
     }
-    if(!token){
+    if (!token) {
         res.status(401)
         throw new Error('not authorized')
     }
 })
-export const isAdmin=AsyncHandler(async(req,res,next)=>{
-    const user=await userModel.findById(req.user.id)
-    if(user.role===1){
-       next()
+export const isAdmin = AsyncHandler(async (req, res, next) => {
+    const user = await userModel.findById(req.user.id)
+    if (user.role === 1) {
+        next()
     }
-    else{
+    else {
         res.status(401);
         throw new Error("user is not admin")
     }
- }
+}
 )
